bf6f8140f7044422f62459d1105d07d607ea5832
Daniel Facini Kiffer <danielfkiffer@outlook.com>
2022-06-30 16:01:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import requests
+import whois11
+import os
+
+arqs = [f for f in os.listdir('.') if os.path.isfile(os.path.join('.', f)) and f.endswith('.txt')]
+
+GREEN = "\033[0;32m"
+RED   = "\033[1;31m"
+mytimeout = (2, 5)
+
+url = input('-> URL\n-> ')
+print('WORDLISTS ->', arqs)
+wrlist = input('-> WORDLIST\n-> ')
+
+if wrlist not in arqs:
+    print("Selecione uma wordlist valida!")
+    print("->", arqs)
+    exit()
+
+else:
+    with open(wrlist, "r") as buster:
+        buster = buster.readlines()
+        buster = list(map(lambda s: s.strip(), buster))
+        bust = buster
+
+    for i in bust:
+        url_check = f'''https://{url}/{i}'''
+        r = requests.get(url_check)
+
+        if r.status_code == 200:
+            print(GREEN,url_check, '[',r.status_code,']')
+        else:
+            continue
+
+    print("\033[0;0m")
+
+    print('WORDLISTS ->', arqs)
+    wrlist_sub = input('-> WORDLISTS SUBDOMAIN\n-> ')      
+
+    with open(wrlist_sub, "r") as buster_subdomain:
+        buster_subdomain = buster_subdomain.readlines()
+        buster_subdomain = list(map(lambda s: s.strip(), buster_subdomain))
+        bustsub = buster_subdomain  
+
+    for x in bustsub:
+        url_check_sub = f'''https://{x}.{url}'''
+        try: 
+            r_sub = requests.get(url_check_sub, timeout=mytimeout)
+            print(GREEN,url_check_sub, "[ACESSADO]")
+        except:
+            continue    
+
+print("\033[0;0m")
+
+print("Criando html da URL..")    
+
+gethmtl = f'''https://{url}'''
+rhtml = requests.get(gethmtl)
+html = rhtml.text
+
+try:
+    with open('save.html', "w", encoding="utf-8") as htmltext:
+        htmltext.write(html)
+        print("HTML criado em save.html")
+except:
+    print("Nao foi possivel criar o arquivo html.")
+
+
+text_whois = input("Deseja procurar info pelo WHOIS11? \n-> Digite S para sim\n-> Digite N para nao\n-> ")
+
+if text_whois not in ['S', 'N']:
+    print("Valor digitado incorretamente..")
+    text_whois = input("Deseja procurar info pelo WHOIS11? \n-> Digite S para sim\n-> Digite N para nao\n-> ")
+
+if text_whois == 'S':
+    model = f'''[MODEL = {url}]'''
+    print(model)
+    print("Pegando informacoes da URL..")
+    print(whois11.whois(url))
+
+if text_whois == 'N':
+    print("Saindo...")
+    exit()
+
---
+++ readme.md
@@ -0,0 +1,31 @@
+Linguagem utilizada 
+
+    Python 3.6
+
+Imports  
+
+    import requests
+    import whois11
+    import os
+
+O que e?
+
+Simulando o dirb ou gobuster usado no kali linux de uma forma recriada em python ele tem a funcao de buscar diretorios e subdominios de um site para buscar informacoes a fim de encontrar vulnerabilidades.
+
+Formato URL
+
+    [SITE.COM.BR]
+
+Como funciona?
+
+Ao iniciar o codigo ele ira perguntar a url que voce deseja buscar
+escolhendo a wordlist para buscar diretorios [/admin, /login, /index] apos finalizar ele vai pedir outra wordlist para buscar subdominios do site [admin.site.com.br, webmail.site.com.br] ao finalizar a busca ele salva o HTML principal da url no arquivo save.html e ao finalizar ele pergunta se quer mostrar informacoes do site como dominio, emails, etc...
+
+
+OBS
+
+    estou disponibilizando 2 wordlists, uma para diretorios e outra para subdominios, voce pode baixar mais e usa-las.
+
+Wordlists
+
+    Salve wordlists na pasta e quando for usar remova os .txt de dentro das pastas.
